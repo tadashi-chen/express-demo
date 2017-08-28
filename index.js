9a8109fed4c8c8af8f7284d38fb7ca12a65c8fdc
ChenJing <tadash-chen@qq.com>
2017-08-28 13:55:45 +0800

    update url

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ app.get('/', (req, res) => {
 	});
 });
 
-app.listen(3000, () => {
-	console.log('listening on port 3000');
+app.listen(4000, () => {
+	console.log('listening on port 4000');
 });
--- router.js
+++ router.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 
 //路由GET请求
-router.post('/ajax', (req, res) => {
+router.post('/api/ajax', (req, res) => {
 	res.send({ msg: 'You sent the message:' + req.body.sendMsg });
 });
 
 //路由POST请求
-router.get('/ajax', (req, res) => {
+router.get('/api/ajax', (req, res) => {
 	res.send({ msg: 'You sent the message:' + req.query.sendMsg });
 });
 
